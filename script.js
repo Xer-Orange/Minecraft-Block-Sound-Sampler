432e46488ac918a71946fd3c16fafb037a0dc334
Xer-Orange <muhammadirshadm7860@gmail.com>
2026-05-24 21:14:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var sound1 = new Audio();
+sound1.src = "Assets/sounds/wood.mp3";
+var sound2 = new Audio();
+sound2.src = "Assets/sounds/Grass_dig1.mp3";
+var sound3 = new Audio();
+sound3.src = "Assets/sounds/sand_dig3.ogg.mp3";
+var sound4 = new Audio();
+sound4.src = "Assets/sounds/lava.mp3";
